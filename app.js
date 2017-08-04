7f00f644bf7c0a07e1258865b108b8cb96525660
chandra sekhar avula <cavula@innominds.com>
2017-08-04 17:02:56 +0530

    commented db creation script

--- app.js
+++ app.js
@@ -44,35 +44,52 @@ http.createServer(app).listen("8001",function(){
 });
 
 
-Sequelize.transaction.then(function(t){
-    models.Role.sync().then(function(){
-    models.User.sync().then(function(){
-        models.AccessLog.sync().then(function(){
-          models.IdType.sync().then(function(){
-            models.QuorumNode.sync().then(function(){
-                models.Institute.sync().then(function(){
-                  models.PasswordRecovery.sync().then(function(){
-                    models.SHLog.sync().then(function(){
-                        models.ServiceProviderMapping.sync().then(function(){
-                            models.UserInstitute.sync().then(function(){
-                              // models.ContractHistory.sync.then(function(){
-                                
-                              // })
-                                  t.commit();
-                            }).catch(function(error){
-                                return t.rollback();
-                            })
-                        })
-                    })
-                  })
-                })
-            })
-          })     
-        })  
-    })
-  });
+// return Sequelize.transaction({type: Sequelize.Transaction.TYPES.EXCLUSIVE}, transaction => {
 
-})// end transaction
+//  // your transactions
+
+// }).then(result => {
+//   // transaction has been committed. Do something after the commit if required.
+// }).catch(err => {
+//   // do something with the err.
+// });
+
+// models.UserInstitute.sync().then(function(){
+//                                   t.commit();
+//                             }).catch(function(error){
+//                                 return t.rollback();
+// })
+
+
+// Sequelize.transaction({type: Sequelize.Transaction.TYPES.EXCLUSIVE}, transaction => {
+//     models.Role.sync().then(function(){
+//     models.User.sync().then(function(){
+//         models.AccessLog.sync().then(function(){
+//           models.IdType.sync().then(function(){
+//             models.QuorumNode.sync().then(function(){
+//                 models.Institute.sync().then(function(){
+//                   models.PasswordRecovery.sync().then(function(){
+//                     models.SHLog.sync().then(function(){
+//                         models.ServiceProviderMapping.sync().then(function(){
+//                             models.UserInstitute.sync().then(function(){
+//                               // models.ContractHistory.sync.then(function(){
+                                
+//                               // })
+//                                   t.commit();
+//                             }).catch(function(error){
+//                                 return t.rollback();
+//                             })
+//                         })
+//                     })
+//                   })
+//                 })
+//             })
+//           })     
+//         })  
+//     })
+//   });
+
+// })// end transaction
 
 
 

f006bc328ca0ede897d4f22daac366ef519b7ea9
chandra sekhar avula <cavula@innominds.com>
2017-08-15 02:59:47 +0530

    "options" headers set .

--- routes/institute.js
+++ routes/institute.js
@@ -7,7 +7,24 @@ router=express.Router();
 
 
 router.post('/', function(req, res) {
-      instituteController.createInstitute(req,res);
+
+      if (req.method === 'OPTIONS') {
+            console.log('!OPTIONS');
+            var headers = {};
+            // IE8 does not allow domains to be specified, just the *
+            // headers["Access-Control-Allow-Origin"] = req.headers.origin;
+            headers["Access-Control-Allow-Origin"] = "*";
+            headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
+            headers["Access-Control-Allow-Credentials"] = false;
+            headers["Access-Control-Max-Age"] = '86400'; // 24 hours
+            headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
+            res.writeHead(200, headers);
+            res.end();
+      } else {
+            instituteController.createInstitute(req,res);
+      }
+
+
 });
 
 router.get('/', function(req, res) {

e3d2de58c7984e1b455fdf8a179d53379c4cded1
chandra sekhar avula <cavula@innominds.com>
2017-08-15 04:04:37 +0530

    app.js

--- app.js
+++ app.js
@@ -29,7 +29,14 @@ var cors = require('cors');
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(logger('dev'));   
+app.use(logger('dev')); 
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+  
 
 app.set('models', require('./app/models'));
 var models = app.get('models');

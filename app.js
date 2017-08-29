6fa198db00b2690e4d17056548d30e4fdb6d2fc8
chandra sekhar avula <cavula@innominds.com>
2017-08-29 15:09:25 +0530

    cors headers added

--- app.js
+++ app.js
@@ -36,9 +36,11 @@ env.initialize(app);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(logger('dev'));
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,Authorization");
+  res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
   next();
 });
 
